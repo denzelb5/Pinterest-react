423106380770bb739bd10513d78b8ec681c55329
Denise Baker <denviol@yahoo.com>
2019-12-18 21:08:39 -0600

    finished update

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -6,12 +6,20 @@ class Board extends React.Component {
   static propTypes = {
     board: boardShape.boardShape,
     setSingleBoard: PropTypes.func,
+    setEditMode: PropTypes.func,
+    setBoardToEdit: PropTypes.func,
   }
 
   setSelectedBoardId = (e) => {
     e.preventDefault();
     const { setSingleBoard, board } = this.props;
     setSingleBoard(board.id);
+  }
+
+  setEditMode = (e) => {
+    e.preventDefault();
+    this.props.setEditMode(true);
+    this.props.setBoardToEdit(this.props.board);
   }
 
 
@@ -24,6 +32,7 @@ class Board extends React.Component {
             <h5 className="card-title">{board.name}</h5>
             <p className="card-text">{board.description}</p>
             <button className="btn btn-primary" onClick={this.setSelectedBoardId}>View Pins</button>
+            <button className="btn btn-danger" onClick={this.setEditMode}>Edit Board</button>
           </div>
         </div>
       </div>
